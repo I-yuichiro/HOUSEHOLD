059ff075bd2e4f234a1160a90446b33065078403
I-yuichiro <52876660+I-yuichiro@users.noreply.github.com>
2020-05-06 15:16:02 +0900

    hou

--- app/models/entries.py
+++ app/models/entries.py
@@ -1,14 +1,13 @@
 from app import db
-from datetime import datetime
 
 class Entry(db.Model):
   __tablename__ = 'entries'
   categories = db.Column(db.Text)
   cost = db.Column(db.Integer, primary_key=True)
 
-  @classmethod
-  def from_args(cls, categories: str, cost: str):
-    instance = cls()
-    instance.categories = categories
-    instance.cost = cost
-    return instance
+  def __init__(self, categories=None, cost=None):
+    self.categories = categories
+    self.cost = cost
+
+  def __repr__(self):
+    return '<Entry id:{} categories:{} cost:{}>'.format(self.cost, self.categories)
--- app/views/hold.py
+++ app/views/hold.py
@@ -1,19 +1,33 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from app import app
 from app import db
-from app.models.entries import Entry, entries
+from app.models.entries import Entry
 
-
+@app.route('/add')
+def show_entries():
+  if not session.get('logged_in'):
+    return redirect(url_for('login'))
+  entries = Entry.query.order_by(Entry.id.desc()).all()
+  return render_template('/index.html', entries=entries) 
+  
+  
 @app.route('/add', methods=['GET', 'POST'])
 def add_entry():
-  flash('新しい項目が追加されました。')
-  return redirect(url_for('index'))
-
+  if not session.get('logged_in'):
+    return redirect(url_for('index'))
   entry = Entry(
     categories = request.form['categories'],
     cost = request.form['cost']
   )
   db.session.add(entry)
   db.session.commit()
+  return redirect(url_for('index'))
+
+@app.route('/index/<int:id>', methods=["GET"])
+def show_entry(id):
+  if not session.get('logged_in'):
+    return redirect(url_for('add'))
+  entry = Entry.query.get(id)
+  return render_template('index.html', entry=entry)
 
   
